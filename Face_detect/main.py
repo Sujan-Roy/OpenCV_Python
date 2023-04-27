c6106b47e2c809506f38930212f41b093ff8d652
unknown <Sujan-Roy>
2023-04-27 13:05:07 +0900

    update main.py

--- Face_detect/main.py
+++ Face_detect/main.py
@@ -4,16 +4,17 @@ face_cascade= cv2.CascadeClassifier('face.xml')
 
 eye_casecade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-for index in range(10):
-    cap = cv2.VideoCapture(index)
-    if cap.isOpened():
-        print(f"Using index {index} for external webcam")
-        break
-    else:
-        cap.release()
+# for index in range(10):
+#     cap = cv2.VideoCapture(index,cv2.CAP_DSHOW)
+#     if cap.isOpened():
+#         print(f"Using index {index} for external webcam")
+#         break
+#     else:
+#         cap.release()
 #result, img = cap.read()
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 
-while 1:
+while True:
     ret, imag = cap.read()
     gray= cv2.cvtColor(imag,cv2.COLOR_BGR2GRAY)
 
@@ -24,10 +25,13 @@ while 1:
                       (x+height,y+width),
                       (255,255,0),5)
         roi_gray= gray[y:y+height, x:x+width]
-        roi_color = img[y:y+height, x:x+width]
+        roi_color = imag[y:y+height, x:x+width]
         eye = eye_casecade.detectMultiScale(roi_gray)
         for(ex,ey,eheight,ewidth) in eye:
             cv2.rectangle(roi_color,(ex,ey),
                           (ex+eheight,ey+ewidth),
                           (255,255,0),2)
-cv2.imshow('img',imag)
+    cv2.imshow('img',imag)
+    cv2.waitKey(1)
+cap.release()
+cv2.destroyAllWindows()
