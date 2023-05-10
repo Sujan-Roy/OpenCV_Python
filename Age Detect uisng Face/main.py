7476e8e40e44e65ea97a81cf97836af4fdebe9b6
unknown <Sujan-Roy>
2023-05-10 19:28:11 +0900

    Update main.py

--- Age Detect uisng Face/main.py
+++ Age Detect uisng Face/main.py
@@ -7,3 +7,13 @@ face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Load the age detector model.
 age_detector = cv2.dnn.readNet('age_net.caffemodel', 'age_deploy.prototxt')
 
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+while True:
+    ret, imag = cap.read()
+    gray= cv2.cvtColor(imag,cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('img',imag)
+    cv2.waitKey(1)
+cap.release()
+cv2.destroyAllWindows()
