73db1c1b6fb832c0894a753109c2538418d81723
unknown <Sujan-Roy>
2023-06-16 15:29:44 +0900

    Add remove background program

---
+++ Remove backgroud from Image/main.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+from rembg import remove
+import matplotlib.pyplot as plt
+
+newPath="Output_Image_directory/"
+
+#Create new directory if the diserd directory is not exist
+if not os.path.exists(newPath):
+    os.makedirs(newPath)
+
+
+image = cv2.imread("dataset/dog.jpg")
+output_image=remove(image)
+
+output_path = os.path.join(newPath, "dog.jpg")
+cv2.imwrite(output_path, output_image)
+
+#output_image.save("newPath/dog.jpg")
+plt.subplot(2,2,1)
+plt.title("Orginal Image")
+plt.imshow(image)
+plt.subplot(2,2,2)
+plt.title("Back Image")
+plt.imshow(output_image)
+
+plt.show()
